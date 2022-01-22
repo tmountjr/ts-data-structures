451ad0c849d4599712e9bda150de0d5f4caa0de6
Tom Mount <tmountjr@gmail.com>
2022-01-21 21:59:27 -0500

    manually create entrypoint file for now

---
+++ index.js
@@ -0,0 +1,24 @@
+const { BinaryTree } = require('./dist/BinaryTree');
+const { Graph } = require('./dist/Graph');
+const { MinHeap, MaxHeap } = require('./dist/Heap');
+const { ListNode, LinkedList } = require('./dist/LinkedList');
+const { bubblesort, quicksort, mergesort, heapsort } = require('./dist/Sorter');
+const { Stack } = require('./dist/Stack');
+const { TreeAsArray } = require('./dist/TreeAsArray');
+const { Trie } = require('./dist/Trie');
+
+module.exports = {
+  BinaryTree,
+  Graph,
+  MinHeap,
+  MaxHeap,
+  ListNode,
+  LinkedList,
+  bubblesort,
+  quicksort,
+  mergesort,
+  heapsort,
+  Stack,
+  TreeAsArray,
+  Trie,
+}
